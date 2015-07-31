52464d8c9fa952ef16b3a9ec3cfb6aae72eda2cb
François Poyer <francois.poyer@gmail.com>
2015-07-31 11:41:03 +0200

    Made input data consistent

--- src/test/java/org/fpoyer/tddasifyoumeantit/TicTacToeTest.java
+++ src/test/java/org/fpoyer/tddasifyoumeantit/TicTacToeTest.java
@@ -11,7 +11,7 @@ public class TicTacToeTest {
 
 	@Test public void 
 	should_win_X_if_A_row_taken() {
-		String sequencePlayed = "A1, B1, A2, B2, A3,";
+		String sequencePlayed = "A1, B1, A2, B2, A3, ";
 		String row = "A";
 		boolean Xwin = xWinsRow(sequencePlayed, row);
 		assertThat(Xwin, is(true));
@@ -19,7 +19,7 @@ public class TicTacToeTest {
 
 	@Test public void 
 	should_win_X_if_B_row_taken() {
-		String sequencePlayed = "B1, A1, B2, A2, B3,";
+		String sequencePlayed = "B1, A1, B2, A2, B3, ";
 		String row = "B";
 		boolean Xwin = xWinsRow(sequencePlayed, row);
 		assertThat(Xwin, is(true));
@@ -27,7 +27,7 @@ public class TicTacToeTest {
 
 	@Test public void 
 	should_win_X_if_C_row_taken() {
-		String sequencePlayed = "C1, A1, C2, A2, C3,";
+		String sequencePlayed = "C1, A1, C2, A2, C3, ";
 		String row = "C";
 		boolean Xwin = xWinsRow(sequencePlayed, row);
 		assertThat(Xwin, is(true));
@@ -35,7 +35,7 @@ public class TicTacToeTest {
 
 	@Test public void 
 	should_not_win_X_if_not_all_row_taken() {
-		String sequencePlayed = "A1";
+		String sequencePlayed = "A1, ";
 		String row = "A";
 		boolean Xwin = xWinsRow(sequencePlayed, row);
 		assertThat(Xwin, is(false));
@@ -43,7 +43,7 @@ public class TicTacToeTest {
 	
 	@Test public void 
 	should_win_X_if_column_1_taken() {
-		String sequencePlayed = "A1, A2, B1, B2, C1";
+		String sequencePlayed = "A1, A2, B1, B2, C1, ";
 		String column = "1";
 		boolean Xwin = xWinsColumn(sequencePlayed, column);
 		assertThat(Xwin, is(true));
@@ -51,7 +51,7 @@ public class TicTacToeTest {
 	
 	@Test public void 
 	should_win_X_if_column_2_taken() {
-		String sequencePlayed = "A2, A1, B2, B1, C2";
+		String sequencePlayed = "A2, A1, B2, B1, C2, ";
 		String column = "2";
 		boolean Xwin = xWinsColumn(sequencePlayed, column);
 		assertThat(Xwin, is(true));
@@ -59,7 +59,7 @@ public class TicTacToeTest {
 	
 	@Test public void 
 	should_win_X_if_column_3_taken() {
-		String sequencePlayed = "A3, A2, B3, B2, C3";
+		String sequencePlayed = "A3, A2, B3, B2, C3, ";
 		String column = "3";
 		boolean Xwin = xWinsColumn(sequencePlayed, column);
 		assertThat(Xwin, is(true));
@@ -67,7 +67,7 @@ public class TicTacToeTest {
 	
 	@Test public void 
 	should_not_win_X_if_not_all_column_taken() {
-		String sequencePlayed = "A1";
+		String sequencePlayed = "A1, ";
 		String column = "1";
 		boolean Xwin = xWinsColumn(sequencePlayed, column);
 		assertThat(Xwin, is(false));
