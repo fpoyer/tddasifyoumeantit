1cec46a4b79205b9dfeefa986b63c41f65612ecb
François Poyer <francois.poyer@gmail.com>
2015-07-31 10:58:29 +0200

    Refactored to avoid duplication and added a third test for C row

--- src/test/java/org/fpoyer/tddasifyoumeantit/TicTacToeTest.java
+++ src/test/java/org/fpoyer/tddasifyoumeantit/TicTacToeTest.java
@@ -11,13 +11,30 @@ public class TicTacToeTest {
 
 	@Test public void 
 	should_win_X_if_A_row_taken() {
-		boolean Xwin = "A1, B1, A2, B2, A3".startsWith("A");
+		String sequencePlayed = "A1, B1, A2, B2, A3,";
+		String row = "A";
+		boolean Xwin = wins(sequencePlayed, row);
 		assertThat(Xwin, is(true));
 	}
-	
+
 	@Test public void 
 	should_win_X_if_B_row_taken() {
-		boolean Xwin = "B1, A1, B2, A2, B3".startsWith("B");
+		String sequencePlayed = "B1, A1, B2, A2, B3,";
+		String row = "B";
+		boolean Xwin = wins(sequencePlayed, row);
 		assertThat(Xwin, is(true));
 	}
+
+	@Test public void 
+	should_win_X_if_C_row_taken() {
+		String sequencePlayed = "C1, A1, C2, A2, C3,";
+		String row = "C";
+		boolean Xwin = wins(sequencePlayed, row);
+		assertThat(Xwin, is(true));
+	}
+
+	private boolean wins(String sequencePlayed, String row) {
+		return sequencePlayed.startsWith(row);
+	}
+	
 }
