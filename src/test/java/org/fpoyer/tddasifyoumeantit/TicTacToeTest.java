030071c79d65b40af0374dbed0729b9d3dd774fd
François Poyer <francois.poyer@gmail.com>
2015-07-31 10:50:25 +0200

    new test for B row, renamed first test as A row

--- src/test/java/org/fpoyer/tddasifyoumeantit/TicTacToeTest.java
+++ src/test/java/org/fpoyer/tddasifyoumeantit/TicTacToeTest.java
@@ -10,8 +10,14 @@ import org.junit.Test;
 public class TicTacToeTest {
 
 	@Test public void 
-	should_win_X_if_row_taken() {
+	should_win_X_if_A_row_taken() {
 		boolean Xwin = "A1, B1, A2, B2, A3".startsWith("A");
+		assertThat(Xwin, is(true));
+	}
+	
+	@Test public void 
+	should_win_X_if_B_row_taken() {
+		boolean Xwin = false;
 		assertThat(Xwin, is(true));
 	}
 }
